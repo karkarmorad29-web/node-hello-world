0bf2058dfe8afb8659f6d1ab8b878160d6cfe081
morad <KARKARMORAD29@GMAIL.COM>
2026-02-13 21:35:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+console.log("Hello, World!");
+
+const user = "Boolean";
+const timestamp = new Date().toLocaleString();
+
+
+console.clear(); // Pulisce il terminale ad ogni riavvio
+
+console.log("===============================");
+console.log(`Messaggio: Hello, ${user}!`);
+console.log(`Ultimo aggiornamento: ${timestamp}`);
+console.log("===============================");
+
+
+
